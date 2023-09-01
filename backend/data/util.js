6d9653433fc63a3ebe0fe0dbdda0b2e051c277d5
Thaw Zin Tun <thawzintun98@gmail.com>
2023-09-01 15:57:41 +0630

    new new

--- backend/data/util.js
+++ backend/data/util.js
@@ -1,12 +1,12 @@
-const fs = require("node:fs/promises");
+const fs = require("fs-promise");
 
 async function readData() {
-  const data = await fs.readFile("posts.json", "utf8");
-  return JSON.parse(data);
+    const data = await fs.readFile("posts.json", "utf8");
+    return JSON.parse(data);
 }
 
 async function writeData(data) {
-  await fs.writeFile("posts.json", JSON.stringify(data));
+    await fs.writeFile("posts.json", JSON.stringify(data));
 }
 
 exports.readData = readData;
