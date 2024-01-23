9df1d982189bfd8d1c160b65514fbfb2906dd05a
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-23 14:24:18 +0630

    a little changes

--- backend/app.js
+++ backend/app.js
@@ -6,22 +6,17 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
 });
 
 app.use(authRoutes);
 
 app.use("/posts", eventRoutes);
-
-app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message || "Something went wrong.";
-  res.status(status).json({ message: message });
-});
 
 app.listen(8080);
