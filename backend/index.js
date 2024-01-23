91b0556a50b9ffa80ac60a93edf22809177a7232
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-24 01:10:22 +0630

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
 });
 
 app.listen(port, () => {
