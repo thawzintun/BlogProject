d8dac941b447a2d186d9899bbc6a311849cdbd24
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-23 22:53:39 +0630

    edit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-
+const app = express();
 const eventRoutes = require("./routes/posts");
 const authRoutes = require("./routes/auth");
 
-const app = express();
+app.use(authRoutes);
+
+app.use("/posts", eventRoutes);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,12 +14,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    next();
 });
-
-app.use(authRoutes);
-
-app.use("/posts", eventRoutes);
 
 app.listen(8080, function () {
     console.log("Your app is listening");
